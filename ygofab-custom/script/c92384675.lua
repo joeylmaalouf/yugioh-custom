4f9bb85cdbebba5b59627cbd50ddf4824a4a6411
joeylmaalouf <joeylmaalouf@gmail.com>
2021-10-23 17:12:40 -0400

    flesh out cards in ygofab toml

--- ygofab-custom/script/c92384675.lua
+++ ygofab-custom/script/c92384675.lua
@@ -1,8 +1,8 @@
---Vylett, Underlord of Dark World
+-- Vylett, Underlord of Dark World
 local s, id = GetID()
 
 function s.initial_effect(c)
 	local e1 = Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DRAW + CATEGORY_HANDES)
-	--TODO
+	-- TODO
 end
