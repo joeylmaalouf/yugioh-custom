9a3c51d6cb32343b8fce473aab44249e1e1c4a4f
joeylmaalouf <joeylmaalouf@gmail.com>
2021-11-05 00:03:11 -0400

    started working on the frozen barrier

---
+++ ygofab-custom/script/c84206436.lua
@@ -0,0 +1,12 @@
+-- The Frozen Barrier
+local s, id = GetID()
+
+function s.initial_effect (c)
+	-- effect 1: card activation
+	local e1 = Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+
+	-- effect 2: TODO
+end
